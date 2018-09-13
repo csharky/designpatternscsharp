937941214f94ed169be52633476a4e70e89633c0
Sir James <james.brody16@hotmail.com>
2018-09-13 12:37:48 +0300

    Added Singleton pattern example

---
+++ DesignPatternsExample/Creational/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsExample.Creational
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        private string content = "Some content inside the singleton";
+
+        private Singleton()
+        {
+            Console.WriteLine("Singleton has been instanced inside the private contsruct");
+        }
+
+        public static Singleton Instance
+        {
+            get { return instance; }
+        }
+
+        public string GetContent()
+        {
+            return content;
+        }
+    }
+}
--- DesignPatternsExample/Program.cs
+++ DesignPatternsExample/Program.cs
@@ -4,9 +4,26 @@ namespace DesignPatternsExample
 {
     class Program
     {
+        private static void DepartPattern(string name, System.Action action)
+        {
+            var firstLine = "--------- " + name + " Example ---------";
+            Console.WriteLine(firstLine);
+
+            action();
+
+            for (int i = 0; i < firstLine.Length; i++)
+                Console.Write("-");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DepartPattern("Singleton", () =>
+            {
+                var content = Creational.Singleton.Instance.GetContent();
+                Console.WriteLine(content);
+            });
+
+            Console.ReadKey();
         }
     }
 }
